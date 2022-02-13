6098e29ca15a0330d04111e1425e259a3e917b4d
Neh Joshi <nehjoshi5@gmail.com>
2022-02-13 22:21:04 +0530

    Heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://nj-socket-io-chat.netlify.app',
         methods: ["GET", "POST"]
     }
 });
