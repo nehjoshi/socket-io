9f6f5f3d5287c1273998f644747f0f113f536dab
Neh Joshi <nehjoshi5@gmail.com>
2022-02-14 17:22:02 +0530

    Better styling

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import ScrollToBottom from 'react-scroll-to-bottom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Picker from 'emoji-picker-react';
-import {faFaceSmile, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+import { faFaceSmile, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 function Chat({ socket, name, id }) {
     const [message, setMessage] = React.useState("");
@@ -61,45 +61,46 @@ function Chat({ socket, name, id }) {
     }, [socket]);
 
     return (
-        <div className="chat-window">
-            <div className="chat-header">
-                <p>Logged in as: {name}</p>
-            </div>
-            <div className="chat-body">
-                <ScrollToBottom className="message-container">
-                    {messages.map((msg) => {
-                        return (
-                            <div className="message" id={name === msg.from ? "you" : "other"} >
-                                <div>
-                                    <div className="message-content">
-                                        <p>{msg.message}</p>
-                                    </div>
-                                    <div className="message-meta">
-                                        <p id="time">{msg.time}</p>
-                                        <p id="author">{msg.from}</p>
+        <>
+            <div className="chat-window">
+                <div className="chat-header">
+                    <p>Logged in as: {name}</p>
+                </div>
+                <div className="chat-body">
+                    <ScrollToBottom className="message-container">
+                        {messages.map((msg) => {
+                            return (
+                                <div className="message" id={name === msg.from ? "you" : "other"} >
+                                    <div>
+                                        <div className="message-content">
+                                            <p>{msg.message}</p>
+                                        </div>
+                                        <div className="message-meta">
+                                            <p id="time">{msg.time}</p>
+                                            <p id="author">{msg.from}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })}
-                    <p id="typing">{typing}</p>
-                </ScrollToBottom>
-            </div>
-            <div className="chat-footer">
-            <FontAwesomeIcon icon={faFaceSmile} className="expand-emoji-icon" onClick={() => setShowPicker(!showPicker)}/>
-                <input
-                    type="text"
-                    value={message}
-                    className="input"
-                    placeholder="Type your message here..."
-                    onChange={handleChangeMessage}
-                    onKeyPress={e => { e.key === "Enter" && SendMessage() }}
-                />
-                <FontAwesomeIcon icon={faPaperPlane} onClick={SendMessage} className="send-icon"/>
+                            )
+                        })}
+                        <p id="typing">{typing}</p>
+                    </ScrollToBottom>
+                </div>
+                <div className="chat-footer">
+                    <FontAwesomeIcon icon={faFaceSmile} className="expand-emoji-icon" onClick={() => setShowPicker(!showPicker)} />
+                    <input
+                        type="text"
+                        value={message}
+                        className="input"
+                        placeholder="Type your message here..."
+                        onChange={handleChangeMessage}
+                        onKeyPress={e => { e.key === "Enter" && SendMessage() }}
+                    />
+                    <FontAwesomeIcon icon={faPaperPlane} onClick={SendMessage} className="send-icon" />
+                </div>
             </div>
             {showPicker && <Picker onEmojiClick={onEmojiClick} />}
-
-        </div>
+        </>
     )
 }
 
